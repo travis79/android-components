3e5b804a5358d3d39f5de624c1defca9226f439d
Travis Long <tlong@mozilla.com>
2020-09-11 08:02:36 -0500

    Add some real tests

--- components/service/nimbus/src/test/java/mozilla/components/service/nimbus/NimbusTest.kt
+++ components/service/nimbus/src/test/java/mozilla/components/service/nimbus/NimbusTest.kt
@@ -7,9 +7,16 @@ package mozilla.components.service.nimbus
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import mozilla.components.service.nimbus.NimbusInternalAPI.Companion.getLanguageFromLocale
+import mozilla.components.service.nimbus.NimbusInternalAPI.Companion.getLocaleTag
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.Locale
 
 @RunWith(AndroidJUnit4::class)
 class NimbusTest {
@@ -18,8 +25,47 @@ class NimbusTest {
 
     @Test
     fun `test Nimbus initialize`() {
-        Nimbus.initialize(context) { experiments ->
-            assertTrue(experiments.count() > 0)
+        runBlocking {
+            try {
+                Nimbus.initialize(context) { experiments ->
+                    assertTrue(experiments.count() > 0)
+                }
+            } catch (e: UnsatisfiedLinkError) {
+                assert(false)
+            }
         }
+    }
+
+    @Test
+    fun `getLanguageTag() reports the tag for the default locale`() {
+        val defaultLanguageTag = getLocaleTag()
+
+        assertNotNull(defaultLanguageTag)
+        assertFalse(defaultLanguageTag.isEmpty())
+        assertEquals("en-US", defaultLanguageTag)
+    }
+
+    @Test
+    fun `getLanguageTag reports the correct tag for a non-default language`() {
+        val defaultLocale = Locale.getDefault()
+
+        try {
+            Locale.setDefault(Locale("fy", "NL"))
+
+            val languageTag = getLocaleTag()
+
+            assertNotNull(languageTag)
+            assertFalse(languageTag.isEmpty())
+            assertEquals("fy-NL", languageTag)
+        } finally {
+            Locale.setDefault(defaultLocale)
+        }
+    }
+
+    @Test
+    fun `getLanguage reports the modern translation for some languages`() {
+        assertEquals("he", getLanguageFromLocale(Locale("iw", "IL")))
+        assertEquals("id", getLanguageFromLocale(Locale("in", "ID")))
+        assertEquals("yi", getLanguageFromLocale(Locale("ji", "ID")))
     }
 }
